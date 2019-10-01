b6a295fdc7627ed958e274bfeb71bbd9d247d5e4
DanilNizkiy <43649879+DanilNizkiy@users.noreply.github.com>
2019-10-01 23:10:04 +0300

    Update docker_build.sh

--- docker_build.sh
+++ docker_build.sh
@@ -4,4 +4,5 @@ cp ./target/demo-0.0.1-SNAPSHOT.jar ./docker_context
 cp ./src/main/resources/application.properties ./docker_context/config/application.properties
 docker build -f docker_context/Dockerfile -t danilnizkiy/test-app:v1.3 .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push danilnizkiy/test-app:v1.3
+REV=`date +"%m-%d-%H-%M"`
+docker push danilnizkiy/test-app:$REV
