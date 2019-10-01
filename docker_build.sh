b3b78a191acd0279b246a0729bade5c4de65f55d
DanilNizkiy <43649879+DanilNizkiy@users.noreply.github.com>
2019-10-01 22:28:06 +0300

    Update docker_build.sh

--- docker_build.sh
+++ docker_build.sh
@@ -3,3 +3,4 @@
 cp ./target/demo-0.0.1-SNAPSHOT.jar ./docker_context
 cp ./src/main/resources/application.properties ./docker_context/config/application.properties
 docker build -f docker_context/Dockerfile -t danilnizkiy/test-app:v1.3 .
+docker push danilnizkiy/test-app:v1.3
