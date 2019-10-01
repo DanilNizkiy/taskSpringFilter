71aeb7c0ead8b440fbec19d86241328bfec71de0
DanilNizkiy <43649879+DanilNizkiy@users.noreply.github.com>
2019-10-01 22:21:59 +0300

    Update docker_build.sh

--- docker_build.sh
+++ docker_build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mvn clean install
+#mvn clean install
 cp ./target/demo-0.0.1-SNAPSHOT.jar ./docker_context
 cp ./src/main/resources/application.properties ./docker_context/config/application.properties
-docker build -f docker_context/Dockerfile -t danilnizkiy/test-app:v1.2 .
+docker build -f docker_context/Dockerfile -t danilnizkiy/test-app:v1.3 .
